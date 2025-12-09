3b7efe09f694d0c65e33d0d836560e72863c66c0
skshetry <18718008+skshetry@users.noreply.github.com>
2025-12-09 14:18:03 +0545

    drop pytest-servers (#165)

--- dvc_azure/tests/conftest.py
+++ dvc_azure/tests/conftest.py
@@ -1,3 +1,14 @@
+import pytest
+
 from dvc.testing.fixtures import *  # noqa: F403
 
 from .fixtures import *  # noqa: F403
+
+
+def pytest_addoption(parser):
+    parser.addoption("--host", action="store", help="Host running azurite.")
+
+
+@pytest.fixture(scope="session")
+def host(request):
+    return request.config.getoption("--host")
